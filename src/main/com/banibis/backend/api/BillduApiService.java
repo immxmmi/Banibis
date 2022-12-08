ed5adb0a69c8d32d45659742905a34f1f6e5754c
Momo <76620405+immxmmi@users.noreply.github.com>
2022-12-08 14:42:27 +0100

    Fixed pom.xml

--- src/main/com/banibis/backend/api/BillduApiService.java
+++ src/main/com/banibis/backend/api/BillduApiService.java
@@ -2,8 +2,8 @@ package main.com.banibis.backend.api;
 
 import main.com.banibis.models.AllClient;
 import main.com.banibis.models.AllDocuments;
-import main.com.banibis.models.Document;
 import main.com.banibis.models.Client;
+import main.com.banibis.models.Document;
 
 public interface BillduApiService {
     AllClient loadAllClients();
